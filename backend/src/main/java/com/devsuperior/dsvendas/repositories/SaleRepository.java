65463785fd82d3a9e3f6e9cd75e86687bb1bf95f
rocketpowers <rocketpowerswillian@gmail.com>
2021-05-06 21:09:42 -0300

    pagination

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.entities.Seller;
 
-public interface SellerRepository extends JpaRepository<Seller, Long> {
+public interface SaleRepository extends JpaRepository<Sale, Long> {
 	
 	
 
