b9655c272af6b8997b7ba6afea4a691585254e75
rocketpowers <rocketpowerswillian@gmail.com>
2021-05-09 18:35:27 -0300

    datatable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,72 +1,70 @@
+import axios from "axios";
+import { useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { useEffect } from 'react';
+import { BASE_URL } from 'utils/request';
+
+
+
 const DataTable = () => {
+    
+    const [page, setPage] = useState<SalePage>({
+
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+
+    });
+
+    useEffect(() => {
+
+        axios.get(`${BASE_URL}/sales?page=2&size=20&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            });
+    }, []);
 
     return (
-      
-<div className="table-responsive">
-    <table className="table table-striped table-sm">
-        <thead>
-            <tr>
-                <th>Data</th>
-                <th>Vendedor</th>
-                <th>Clientes visitados</th>
-                <th>Negócios fechados</th>
-                <th>Valor</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
 
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+                    {page.content?.map(item => (
 
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+                        <tr key={item.id}>
 
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+                            <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
 
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+                            <td>{item.seller.name}</td>
 
-        </tbody>
-    </table>
-</div>
-           
+                            <td>{item.visited}</td>
+
+                            <td>{item.deals}</td>
+
+                            <td>{item.amount.toFixed(2)}</td>
+
+                        </tr>
+
+                    ))}
+
+                </tbody>
+            </table>
+        </div>
+
     );
-  }
-  
-  export default DataTable;
+}
+
+export default DataTable;
